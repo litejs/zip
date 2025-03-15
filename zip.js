0638ac3ee2f45a09939746184e42147c45319780
Lauri Rooden <lauri@rooden.ee>
2025-03-15 09:06:14 +0200

    Cleanup

--- zip.js
+++ zip.js
@@ -22,7 +22,7 @@
 		add(next.bind(next, null))
 
 		function add(resolve) {
-			k = files[i]
+			k = files[i++]
 			if (!k) {
 				k = files.length
 				ent.push(toUint(cd + le32(0x06054b50) + le32(0) + le32((k<<16) + k) + le32(cd.length) + le32(offset) + "\0\0"))
@@ -35,16 +35,14 @@
 
 			if (typeof file === "string") file = toUint(unescape(encodeURIComponent(file)))
 			var rawSize = file.length
-			, time = dosDate(new Date(k.time || now))
 
 			for (j = 0; j < rawSize; ) {
 				crc = (crc >>> 8) ^ crcTable[(crc ^ file[j++]) & 0xff]
 			}
 			compress(file, function(compressed, method) {
-				var header = le32(20 | 1<<27) + method + le32(time) + le32(-1^crc >>> 0) + le32(compressed.length) + le32(rawSize) + le32(nameLen)
+				var header = le32(20 | 1<<27) + method + le32(dosDate(new Date(k.time || now))) + le32(-1^crc >>> 0) + le32(compressed.length) + le32(rawSize) + le32(nameLen)
 				ent.push(toUint("PK\3\4" + header + name), compressed)
 				cd += "PK\1\2\0\24" + header + "\0\0" + le32(0) + le32(32) + le32(offset) + name
-				i++
 				offset += 30 + compressed.length + nameLen
 				add(resolve)
 			})
@@ -65,7 +63,7 @@
 		return date.getSeconds() >> 1 | date.getMinutes() << 5 | date.getHours() << 11 | date.getDate() << 16 | (date.getMonth() + 1) << 21 | (date.getFullYear() - 1980) << 25
 	}
 	function le32(n) {
-		return String.fromCharCode.call(null, n & 0xff, (n >>> 8) & 0xff, (n >>> 16) & 0xff, (n >>> 24) & 0xff)
+		return String.fromCharCode(n & 0xff, (n >>> 8) & 0xff, (n >>> 16) & 0xff, (n >>> 24) & 0xff)
 	}
 
 	function toUint(str) {
